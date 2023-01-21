eca9d62d54b5f2c6af93cf6a8ad2b6562136c764
Sheraz Shaabir <121964811+sherazshabbir@users.noreply.github.com>
2023-01-21 20:51:42 +0500

    Add files via upload

---
+++ gpacalculator/gpacalculator/Form1.Designer.cs
@@ -0,0 +1,425 @@
+﻿namespace gpacalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.txts1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txts2 = new System.Windows.Forms.TextBox();
+            this.txts3 = new System.Windows.Forms.TextBox();
+            this.txts4 = new System.Windows.Forms.TextBox();
+            this.txts5 = new System.Windows.Forms.TextBox();
+            this.txts6 = new System.Windows.Forms.TextBox();
+            this.txtu1 = new System.Windows.Forms.ComboBox();
+            this.txtu2 = new System.Windows.Forms.ComboBox();
+            this.txtu3 = new System.Windows.Forms.ComboBox();
+            this.txtu4 = new System.Windows.Forms.ComboBox();
+            this.txtu6 = new System.Windows.Forms.ComboBox();
+            this.txtu5 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.txttotal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(144, 323);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(268, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate GPA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(391, 54);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // txts1
+            // 
+            this.txts1.Location = new System.Drawing.Point(144, 50);
+            this.txts1.Name = "txts1";
+            this.txts1.Size = new System.Drawing.Size(40, 20);
+            this.txts1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "OOP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "DLD";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "English";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(65, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Pak study";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(65, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Islamiyat";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(65, 279);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "DS";
+            // 
+            // txts2
+            // 
+            this.txts2.Location = new System.Drawing.Point(144, 92);
+            this.txts2.Name = "txts2";
+            this.txts2.Size = new System.Drawing.Size(40, 20);
+            this.txts2.TabIndex = 10;
+            // 
+            // txts3
+            // 
+            this.txts3.Location = new System.Drawing.Point(144, 141);
+            this.txts3.Name = "txts3";
+            this.txts3.Size = new System.Drawing.Size(40, 20);
+            this.txts3.TabIndex = 11;
+            // 
+            // txts4
+            // 
+            this.txts4.Location = new System.Drawing.Point(144, 192);
+            this.txts4.Name = "txts4";
+            this.txts4.Size = new System.Drawing.Size(40, 20);
+            this.txts4.TabIndex = 12;
+            // 
+            // txts5
+            // 
+            this.txts5.Location = new System.Drawing.Point(144, 229);
+            this.txts5.Name = "txts5";
+            this.txts5.Size = new System.Drawing.Size(40, 20);
+            this.txts5.TabIndex = 13;
+            // 
+            // txts6
+            // 
+            this.txts6.Location = new System.Drawing.Point(144, 272);
+            this.txts6.Name = "txts6";
+            this.txts6.Size = new System.Drawing.Size(40, 20);
+            this.txts6.TabIndex = 14;
+            // 
+            // txtu1
+            // 
+            this.txtu1.DisplayMember = "1,2,3,4";
+            this.txtu1.FormattingEnabled = true;
+            this.txtu1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4"});
+            this.txtu1.Location = new System.Drawing.Point(264, 50);
+            this.txtu1.Name = "txtu1";
+            this.txtu1.Size = new System.Drawing.Size(53, 21);
+            this.txtu1.TabIndex = 16;
+            // 
+            // txtu2
+            // 
+            this.txtu2.DisplayMember = "1,2,3,4";
+            this.txtu2.FormattingEnabled = true;
+            this.txtu2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4"});
+            this.txtu2.Location = new System.Drawing.Point(264, 92);
+            this.txtu2.Name = "txtu2";
+            this.txtu2.Size = new System.Drawing.Size(53, 21);
+            this.txtu2.TabIndex = 17;
+            // 
+            // txtu3
+            // 
+            this.txtu3.DisplayMember = "1,2,3,4";
+            this.txtu3.FormattingEnabled = true;
+            this.txtu3.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4"});
+            this.txtu3.Location = new System.Drawing.Point(264, 141);
+            this.txtu3.Name = "txtu3";
+            this.txtu3.Size = new System.Drawing.Size(53, 21);
+            this.txtu3.TabIndex = 18;
+            // 
+            // txtu4
+            // 
+            this.txtu4.DisplayMember = "1,2,3,4";
+            this.txtu4.FormattingEnabled = true;
+            this.txtu4.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4"});
+            this.txtu4.Location = new System.Drawing.Point(264, 184);
+            this.txtu4.Name = "txtu4";
+            this.txtu4.Size = new System.Drawing.Size(53, 21);
+            this.txtu4.TabIndex = 19;
+            // 
+            // txtu6
+            // 
+            this.txtu6.DisplayMember = "1,2,3,4";
+            this.txtu6.FormattingEnabled = true;
+            this.txtu6.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4"});
+            this.txtu6.Location = new System.Drawing.Point(264, 271);
+            this.txtu6.Name = "txtu6";
+            this.txtu6.Size = new System.Drawing.Size(53, 21);
+            this.txtu6.TabIndex = 20;
+            // 
+            // txtu5
+            // 
+            this.txtu5.DisplayMember = "1,2,3,4";
+            this.txtu5.FormattingEnabled = true;
+            this.txtu5.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4"});
+            this.txtu5.Location = new System.Drawing.Point(264, 229);
+            this.txtu5.Name = "txtu5";
+            this.txtu5.Size = new System.Drawing.Size(53, 21);
+            this.txtu5.TabIndex = 21;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(140, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 21);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Marks";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(247, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(100, 21);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "Credit Hrs";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(387, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(96, 21);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "Withdraw";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(391, 91);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 25;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(391, 137);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(80, 17);
+            this.checkBox3.TabIndex = 26;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(391, 184);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(80, 17);
+            this.checkBox4.TabIndex = 27;
+            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(391, 225);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(80, 17);
+            this.checkBox5.TabIndex = 28;
+            this.checkBox5.Text = "checkBox5";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(391, 272);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(80, 17);
+            this.checkBox6.TabIndex = 29;
+            this.checkBox6.Text = "checkBox6";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // txttotal
+            // 
+            this.txttotal.Location = new System.Drawing.Point(144, 368);
+            this.txttotal.Name = "txttotal";
+            this.txttotal.Size = new System.Drawing.Size(268, 20);
+            this.txttotal.TabIndex = 30;
+            this.txttotal.TextChanged += new System.EventHandler(this.txttotal_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 450);
+            this.Controls.Add(this.txttotal);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtu5);
+            this.Controls.Add(this.txtu6);
+            this.Controls.Add(this.txtu4);
+            this.Controls.Add(this.txtu3);
+            this.Controls.Add(this.txtu2);
+            this.Controls.Add(this.txtu1);
+            this.Controls.Add(this.txts6);
+            this.Controls.Add(this.txts5);
+            this.Controls.Add(this.txts4);
+            this.Controls.Add(this.txts3);
+            this.Controls.Add(this.txts2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txts1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox txts1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txts2;
+        private System.Windows.Forms.TextBox txts3;
+        private System.Windows.Forms.TextBox txts4;
+        private System.Windows.Forms.TextBox txts5;
+        private System.Windows.Forms.TextBox txts6;
+        private System.Windows.Forms.ComboBox txtu1;
+        private System.Windows.Forms.ComboBox txtu2;
+        private System.Windows.Forms.ComboBox txtu3;
+        private System.Windows.Forms.ComboBox txtu4;
+        private System.Windows.Forms.ComboBox txtu6;
+        private System.Windows.Forms.ComboBox txtu5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.TextBox txttotal;
+    }
+}
+
---
+++ gpacalculator/gpacalculator/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gpacalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int m1, m2, m3, m4, m5, m6;
+            int u1, u2, u3, u4, u5, u6;
+            m1 = int.Parse(txts1.Text);
+            m2 = int.Parse(txts2.Text);
+            m3 = int.Parse(txts3.Text);
+            m4 = int.Parse(txts4.Text);
+            m5 = int.Parse(txts5.Text);
+            m6 = int.Parse(txts6.Text);
+            u1 = int.Parse(txtu1.Text);
+            u2 = int.Parse(txtu2.Text);
+            u3 = int.Parse(txtu3.Text);
+            u4 = int.Parse(txtu4.Text);
+            u5 = int.Parse(txtu5.Text);
+            u6 = int.Parse(txtu6.Text);
+            if (checkBox1.Checked)
+            {
+                m1 = 0;
+
+            }
+            if (checkBox2.Checked)
+            {
+                m2 = 0;
+            }
+            if (checkBox3.Checked)
+            {
+                m3 = 0;
+            }
+            if (checkBox4.Checked)
+            {
+                m4 = 0;
+            }
+            if (checkBox5.Checked)
+            {
+                m5 = 0;
+            }
+            if (checkBox6.Checked)
+            {
+                m6 = 0;
+            }
+
+
+
+
+            int cgp = point(m1) * u1 + point(m2) * u2 + point(m3) * u3 + point(m4) * u4 + point(m5) * u5 + point(m6) * u6;
+            int tu = u1 + u2 + u3 + u4 + u5 + u6;
+            int ggpa = cgp / tu;
+            txttotal.Text= ggpa.ToString();
+
+        }
+
+
+        int point(int mark)
+        {
+            int p = 0;
+            if (mark < 40)
+            {
+                p = 0;
+            }
+            else if (mark >= 40 && mark < 50)
+            {
+                p = 1;
+            }
+
+            else if (mark >= 50 && mark < 60)
+            {
+                p = 2;
+            }
+
+            else if (mark >= 60 && mark < 70)
+            {
+                p = 3;
+            }
+
+            else if (mark >= 70 && mark < 80)
+            {
+                p = 4;
+            }
+            else if (mark >= 80 && mark < 100)
+            {
+                p = 5;
+            }
+            return p;
+        }
+
+        private void txttotal_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+    }
